420b2c8a1c43582a3594ceb9e2df5c8b75c761eb
techtheriac <mavyjayann@gmail.com>
2020-04-28 16:59:47 +0100

    Notes Added to ReadMe

--- src/Main.purs
+++ src/Main.purs
@@ -13,10 +13,31 @@ addMe x y = x + y
 addThree :: Int -> Int
 addThree = addMe 3
 
+-- Basic Data Constructor
 data Foo = Foo | Bar String
 runFoo :: Foo -> String
 runFoo Foo = "Damn, right its FoO"
 runFoo (Bar s) = "Yeah it's Bar and " <> s
+
+-- Pattern Matching
+nonSense :: Int -> Int -> Int 
+nonSense n 0 = 0
+-- K = λx.λy.y
+nonSense n _ = n
+
+whoIsGreater :: Int -> Int -> Int
+whoIsGreater x y | x > y = x
+                 | otherwise = y
+
+isEmpty :: forall a. Array a -> Boolean
+isEmpty [] = true
+isEmpty _ = false
+
+-- Type Alias 
+type PersonRec = 
+  { name :: String
+  , age  :: Int
+  }
 
 
 main :: Effect Unit
